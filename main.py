18266fb9e9076f0ffe9b380fe0db273c6d6eb3be
jeetElgoss <114898297+jeetElgoss@users.noreply.github.com>
2025-05-08 09:20:16 +0530

    Update main.py
    comment main drive of production server

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app = Flask(__name__)
 app.register_blueprint(qr_bp)
 app.register_blueprint(intern_bp)
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#     app.run(debug=True, host='0.0.0.0', port=5000)
